3d652d0e9950ddae7960c5796696ccacaee5bce9
Philip-Owen <31570473+Philip-Owen@users.noreply.github.com>
2018-01-20 10:32:18 -0600

    added ng-app and ng-controller to index and client files.

---
+++ server/public/scripts/client.js
@@ -0,0 +1,6 @@
+const galleryApp = angular.module('galleryApp', []);
+
+galleryApp.controller('GalleryController', function() {
+    console.log('in GalleryController');
+    const self = this;
+});
