f5ff4a4dc619a35850df1c396540a6f45314edf9
Philip-Owen <31570473+Philip-Owen@users.noreply.github.com>
2018-01-20 18:49:39 -0600

    added get route on client side to receive images from db

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,47 +1,15 @@
 const galleryApp = angular.module('galleryApp', []);
 
-galleryApp.controller('GalleryController', function() {
+galleryApp.controller('GalleryController', ['$http', function($http) {
     console.log('in GalleryController');
     const self = this;
 
-    self.imageArray = [
-        {
-            url:'images/bwca.jpg',
-            comments: 'Photo from the BWCA. Reminds me of spending time with my family.',
-            like_count: 0,
-            view_count: 0
-        },
-        {
-            url:'images/mpls.jpg',
-            comments: 'Taken from the Stone Arch Bridge. Reminds me how much Minnesota has become a part of my life.',
-            like_count: 0,
-            view_count: 0
-        },
-        {
-            url:'images/friends.jpg',
-            comments: 'Reminds me of good times with friends.',
-            like_count: 0,
-            view_count: 0
-        },
-        {
-            url:'images/halsey.jpg',
-            comments: 'With all good memories, there\'s always a dog. This is Halsey.',
-            like_count: 0,
-            view_count: 0
-        },
-        {
-            url:'images/lincoln.jpg',
-            comments: 'Photo of my ship moored offshore outside of Antalya, Turkey. Reminds me of my time in the service.',
-            like_count: 0,
-            view_count: 0
-        },
-        {
-            url:'images/seattle.jpg',
-            comments: 'Taken from the Puget Sound.',
-            like_count: 0,
-            view_count: 0
-        }
-    ];
+    self.imageArray = [];
+
+    $http.get('/images').then(function(response) {
+        console.log(response);
+        self.imageArray = response.data
+    });
 
     self.flipImage = function(context) {
         context.imageClick = context.imageClick === true ? false: true;
@@ -57,4 +25,4 @@ galleryApp.controller('GalleryController', function() {
         ++context.image.like_count
         console.log(context.image.like_count);
     }
-});
+}]);
